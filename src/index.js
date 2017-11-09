e10f99edae89361d4a721a16c9b0eeb12cefcb23
zeeroday <zeeroday@debian.home>
2017-11-09 19:37:24 +0100

    update index.js file, can't resolve native-react solved

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 //import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
 registerServiceWorker();
+  
